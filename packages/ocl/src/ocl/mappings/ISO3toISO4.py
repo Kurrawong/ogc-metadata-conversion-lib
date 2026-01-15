77d0a7e3cd874fd8677d36fa74e2bb2553d70827
jamiefeiss <jamie.feiss@gmail.com>
2026-01-15 12:26:55 +1000

    Improved ISO 19115-3 XML data quality mapping

--- packages/ocl/src/ocl/mappings/ISO3toISO4.py
+++ packages/ocl/src/ocl/mappings/ISO3toISO4.py
@@ -3,144 +3,204 @@ from ocl.models.mapped.iso3 import ISO3
 from ocl.models.mapped.iso4 import ISO4
 
 
-def process_report(dq: list | dict | None, source: dict) -> list[dict] | None:
+def character_string(obj: dict) -> str | None:
+    valid_keys = [
+        "gco:CharacterString",
+        "gcx:Anchor",
+    ]
+    for key in obj.keys():
+        if key in valid_keys:
+            return obj[key]
+    return None
+
+
+def md_identifier(obj: dict) -> dict:
+    id_obj = {"code": character_string(obj["mcc:code"])}
+
+    if "mcc:authority" in obj:
+        id_obj["authority"] = ci_citation(obj["mcc:authority"]["cit:CI_Citation"])
+
+    if "mcc:codeSpace" in obj:
+        id_obj["codeSpace"] = character_string(obj["mcc:codeSpace"])
+
+    if "mcc:version" in obj:
+        id_obj["version"] = character_string(obj["mcc:version"])
+
+    if "mcc:description" in obj:
+        id_obj["description"] = character_string(obj["mcc:description"])
+
+    return id_obj
+
+
+def ci_citation(obj: dict) -> dict:
+    cit_obj = {"title": character_string(obj["cit:title"])}
+
+    if "cit:alternateTitle" in obj:
+        cit_obj["alternateTitle"] = [character_string(name) for name in
+                                     obj["cit:alternateTitle"]] if isinstance(obj["cit:alternateTitle"],
+                                                                              list) else [
+            character_string(obj["cit:alternateTitle"])]
+
+    if obj.get("cit:date"):
+        cit_obj["date"] = ci_date(obj["cit:date"])
+
+    # edition
+
+    # identifier
+
+    # citedResponsibleParty
+
+    # presentationForm
+
+    # series
+
+    # otherCitationDetails
+
+    # ISBN
+
+    # ISSN
+
+    # onlineResource
+
+    return cit_obj
+
+
+def md_scope(obj: dict | None) -> dict | None:
+    if obj is None:
+        return None
+    scope_obj = {"level": obj["mcc:level"]["mcc:MD_ScopeCode"]["#text"]}
+    # extent
+    # levelDescription
+    return scope_obj
+
+
+def process_report(report: list | dict | None) -> list[dict] | None:
+    if report is None:
+        return None
+
+    if not isinstance(report, list):
+        report = [report]
+
+    report_arr = []
+
+    report_types = [
+        "mdq:DQ_AbsolutePositionalAccuracy",
+        "mdq:DQ_AccuracyOfATimeMeasurement",
+        "mdq:DQ_Commission",
+        "mdq:DQ_ConceptualConsistency",
+        "mdq:DQ_Confidence",
+        "mdq:DQ_DomainConsistency",
+        "mdq:DQ_FormatConsistency",
+        "mdq:DQ_GriddedDataPositionalAccuracy",
+        "mdq:DQ_Homogeneity",
+        "mdq:DQ_NonQuantitativeAttributeCorrectness",
+        "mdq:DQ_Omission",
+        "mdq:DQ_QuantitativeAttributeAccuracy",
+        "mdq:DQ_RelativePositionalAccuracy",
+        "mdq:DQ_Representativity",
+        "mdq:DQ_TemporalConsistency",
+        "mdq:DQ_TemporalValidity",
+        "mdq:DQ_ThematicClassificationCorrectness",
+        "mdq:DQ_TopologicalConsistency",
+    ]
+
+    for r in report:
+        top_key = list(r.keys())[0]
+        if not top_key in report_types:
+            continue
+        r_obj = {"type": top_key.replace("mdq:DQ_", "")}
+
+        measure = r[top_key]["mdq:measure"]["mdq:DQ_MeasureReference"]
+
+        measure_obj = {}
+
+        if "mdq:measureIdentification" in measure:
+            measure_obj["measureIdentification"] = md_identifier(
+                measure["mdq:measureIdentification"]["mcc:MD_Identifier"])
+
+        if "mdq:nameOfMeasure" in measure:
+            measure_obj["nameOfMeasure"] = [character_string(name) for name in
+                                            measure["mdq:nameOfMeasure"]] if isinstance(measure["mdq:nameOfMeasure"],
+                                                                                        list) else [
+                character_string(measure["mdq:nameOfMeasure"])]
+
+        if "mdq:measureDescription" in measure:
+            measure_obj["measureDescription"] = character_string(measure["mdq:measureDescription"])
+
+        r_obj["measure"] = measure_obj
+
+        # result
+        if r[top_key].get("mdq:result"):
+            results = r[top_key]["mdq:result"] if isinstance(r[top_key]["mdq:result"], list) else [
+                r[top_key]["mdq:result"]]
+            result_types = [
+                "mdq:DQ_ConformanceResult",
+                "mdq:DQ_CoverageResult",
+                "mdq:DQ_DescriptiveResult",
+                "mdq:DQ_QuantitativeResult",
+            ]
+
+            results_array = []
+
+            for result in results:
+                result_top_key = list(result.keys())[0]
+                if not result_top_key in result_types:
+                    continue
+                result_obj = {
+                    "type": result_top_key.replace("mdq:DQ_", ""),
+                    "pass": bool(result[result_top_key]["mdq:pass"]["gco:Boolean"])
+                }
+
+                # dateTime
+
+                # resultScope
+
+                # specification (required)
+                result_obj["specification"] = ci_citation(
+                    result[result_top_key]["mdq:specification"]["cit:CI_Citation"])
+
+                # explanation
+
+                results_array.append(result_obj)
+
+            r_obj["result"] = results_array
+
+        # derivedElement
+
+        # evaluationMethod
+
+        report_arr.append(r_obj)
+
+    return report_arr
+
+
+def process_data_quality(dq: list | dict | None) -> list[dict] | None:
     if dq is None:
         return None
-    # need to handle outer dataquality array due to bug in handling @. array selectors for now
+    if not isinstance(dq, list):
+        dq = [dq]
+    d_arr = []
     for d in dq:
-        report = d.get("report")
-        if report is None:
-            continue
-        if not isinstance(report, list):
-            report = [report]
+        d_obj = {"scope": md_scope(d["mdq:DQ_DataQuality"]["mdq:scope"]["mcc:MD_Scope"])}
 
-        report_arr = []
+        if d["mdq:DQ_DataQuality"].get("mdq:report"):
+            d_obj["report"] = process_report(d["mdq:DQ_DataQuality"]["mdq:report"])
 
-        report_types = [
-            "mdq:DQ_AbsolutePositionalAccuracy",
-            "mdq:DQ_AccuracyOfATimeMeasurement",
-            "mdq:DQ_Commission",
-            "mdq:DQ_ConceptualConsistency",
-            "mdq:DQ_Confidence",
-            "mdq:DQ_DomainConsistency",
-            "mdq:DQ_FormatConsistency",
-            "mdq:DQ_GriddedDataPositionalAccuracy",
-            "mdq:DQ_Homogeneity",
-            "mdq:DQ_NonQuantitativeAttributeCorrectness",
-            "mdq:DQ_Omission",
-            "mdq:DQ_QuantitativeAttributeAccuracy",
-            "mdq:DQ_RelativePositionalAccuracy",
-            "mdq:DQ_Representativity",
-            "mdq:DQ_TemporalConsistency",
-            "mdq:DQ_TemporalValidity",
-            "mdq:DQ_ThematicClassificationCorrectness",
-            "mdq:DQ_TopologicalConsistency",
-        ]
+        # qualityEvaluationReport
 
-        for r in report:
-            top_key = list(r.keys())[0]
-            if not top_key in report_types:
-                continue
-            r_obj = {"type": top_key.replace("mdq:DQ_", "")}
-
-            measure = r[top_key]["mdq:measure"]["mdq:DQ_MeasureReference"]
-
-            measure_obj = {}
-
-            if "mdq:measureIdentification" in measure:
-                id = measure["mdq:measureIdentification"]["mcc:MD_Identifier"]
-                id_obj = {"code": id["mcc:code"]["gco:CharacterString"]}
-
-                if "mcc:authority" in id:
-                    auth = id["mcc:authority"]["cit:CI_Citation"]
-                    auth_obj = {"title": auth["cit:title"]["gco:CharacterString"]}
-
-                    if "cit:alternateTitle" in auth:
-                        auth_obj["alternateTitle"] = [name["gco:CharacterString"] for name in auth["cit:alternateTitle"]] if isinstance(auth["cit:alternateTitle"], list) else [auth["cit:alternateTitle"]["gco:CharacterString"]]
-
-                    if auth.get("cit:date"):
-                        auth_obj["date"] = process_dateinfo(auth["cit:date"], {})
-
-                    # TODO: the rest of the authority object
-
-                    id_obj["authority"] = auth_obj
-
-                if "mcc:codeSpace" in id:
-                    id_obj["codeSpace"] = id["mcc:codeSpace"]["gco:CharacterString"]
-
-                if "mcc:version" in id:
-                    id_obj["version"] = id["mcc:version"]["gco:CharacterString"]
-
-                if "mcc:description" in id:
-                    id_obj["description"] = id["mcc:description"]["gco:CharacterString"]
-
-                measure_obj["measureIdentification"] = id_obj
-
-            if "mdq:nameOfMeasure" in measure:
-                measure_obj["nameOfMeasure"] = [name["gco:CharacterString"] for name in measure["mdq:nameOfMeasure"]] if isinstance(measure["mdq:nameOfMeasure"], list) else [measure["mdq:nameOfMeasure"]["gco:CharacterString"]]
-
-            if "mdq:measureDescription" in measure:
-                measure_obj["measureDescription"] = measure["mdq:measureDescription"]["gco:CharacterString"]
-
-            r_obj["measure"] = measure_obj
-
-            # result
-            if r[top_key].get("mdq:result"):
-                results = r[top_key]["mdq:result"] if isinstance(r[top_key]["mdq:result"], list) else [r[top_key]["mdq:result"]]
-                result_types = [
-                    "mdq:DQ_ConformanceResult",
-                    "mdq:DQ_CoverageResult",
-                    "mdq:DQ_DescriptiveResult",
-                    "mdq:DQ_QuantitativeResult",
-                ]
-
-                results_array = []
-
-                for result in results:
-                    result_top_key = list(result.keys())[0]
-                    print(result_top_key)
-                    if not result_top_key in result_types:
-                        continue
-                    result_obj = {
-                        "type": result_top_key.replace("mdq:DQ_", ""),
-                        "pass": bool(result[result_top_key]["mdq:pass"]["gco:Boolean"])
-                    }
-
-                    # dateTime
-
-                    # resultScope
-
-                    # specification (required)
+        d_arr.append(d_obj)
+    return d_arr
 
 
-                    # explanation
-
-                    results_array.append(result_obj)
-
-                r_obj["result"] = results_array
-
-
-            # derivedElement
-
-            # evaluationMethod
-
-            report_arr.append(r_obj)
-
-        d["report"] = report_arr
-    return dq
-
-
-def process_dateinfo(dateinfo: list | dict | None, source: dict) -> dict | None:
-    if dateinfo is None:
+def ci_date(obj: list | dict | None) -> dict | None:
+    if obj is None:
         return None
-    date = dateinfo.get("cit:CI_Date")
-    if date is None:
-        return None
-    if not isinstance(date, list):
-        date = [date]
+    if not isinstance(obj, list):
+        obj = [obj]
     date_obj = {}
-    for d in date:
-        date_obj[d["cit:dateType"]["cit:CI_DateTypeCode"]["#text"]] = d["cit:date"]["gco:DateTime"]
+    for d in obj:
+        date_obj[d["cit:CI_Date"]["cit:dateType"]["cit:CI_DateTypeCode"]["#text"]] = d["cit:CI_Date"]["cit:date"][
+            "gco:DateTime"]
     return date_obj
 
 
@@ -149,26 +209,24 @@ iso3_to_iso4_mapping: MappingDict = {
     "target_model": ISO4,
     "mappings": [
         {
-            "key": "mdb:metadataIdentifier.mcc:MD_Identifier.mcc:code.gco:CharacterString",
+            "key": "mdb:metadataIdentifier.mcc:MD_Identifier.mcc:code",
             "to": "id",
+            "to_func": lambda value, source: character_string(value)
         },
         {
-            "key": "mdb:metadataIdentifier.mcc:MD_Identifier.mcc:code.gco:CharacterString",
-            "to": "properties.metadataIdentifier.code",
+            "key": "mdb:metadataIdentifier.mcc:MD_Identifier",
+            "to": "properties.metadataIdentifier",
+            "to_func": lambda value, source: md_identifier(value)
         },
         {
-            "key": "mdb:dataQualityInfo.mdq:DQ_DataQuality@.mdq:scope.mcc:MD_Scope.mcc:level.mcc:MD_ScopeCode.#text",
-            "to": "properties.dataQualityInfo@.scope.level",
-        },
-        {
-            "key": "mdb:dataQualityInfo.mdq:DQ_DataQuality@.mdq:report",
-            "to": "properties.dataQualityInfo@.report",
-            "to_func": lambda value, source: process_report(value, source)
+            "key": "mdb:dataQualityInfo",
+            "to": "properties.dataQualityInfo",
+            "to_func": lambda value, source: process_data_quality(value)
         },
         {
             "key": "mdb:dateInfo",
             "to": "properties.dateInfo",
-            "to_func": lambda value, source: process_dateinfo(value, source)
+            "to_func": lambda value, source: ci_date(value)
         },
     ],
 }
--- packages/ocl/src/ocl/models/mapped/iso4.py
+++ packages/ocl/src/ocl/models/mapped/iso4.py
@@ -5,6 +5,10 @@ from pydantic import BaseModel, Field, AnyUrl
 
 class ISO4(BaseModel):
     type: Literal["Feature"] = "Feature"
-    conformsTo: list[AnyUrl] = []
+    conformsTo: list[str] = [
+        "https://standards.iso.org/iso/19115/-4/0.1/core",
+        "https://standards.iso.org/iso/19115/-4/0.1/data-quality-core",
+        "https://standards.iso.org/iso/19115/-4/0.1/no-ref"
+    ]
     id: Optional[float | str]
     properties: dict
